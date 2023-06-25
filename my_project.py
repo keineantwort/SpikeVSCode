0161b671b7b42d4bda4da55fc9faf60d3b7c04af
Martin Schumacher <msc@keineantwort.de>
2023-06-25 20:04:15 +0200

    adding a little bit of documentation

--- my_project.py
+++ my_project.py
@@ -1,9 +1,17 @@
+# LEGO type:standard slot:1 autostart
+# ^
+# |
+# +- Autostart Header. The program will autostart in slot one as a standard-typed program
+
+# import all the LEGO APIs. Adjust as you need
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
-
+# connecting to the hub
 hub = PrimeHub()
 
+# display a happy face
 hub.light_matrix.show_image('HAPPY')
     
+# try to run it with the upload button
